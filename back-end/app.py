d33c247111c06ff9d680339ec5c8ad90cab5d7ff
Ronak <rroonnaakk7@gmail.com>
2024-05-05 23:58:09 +0530

    ref: adding server to package.json

--- back-end/app.py
+++ back-end/app.py
@@ -2,12 +2,12 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-load_model = pickle.load(open('NPAfinal_model.sav', 'rb'))
+# load_model = pickle.load(open('/NPAfinal_model.sav', 'rb'))
+load_model = pickle.load(open('D:/Professional/Other/Study/Fake News Detection/back-end/NPAfinal_model.sav', 'rb'))
     
-# @app.route("/")
-# def hello():
-#     return render_template("index.html")
-
+@app.route("/")
+def hello():
+    return render_template("index.html")
 
 @app.route("/predicted", methods=['POST'])
 def predicted():
@@ -15,7 +15,7 @@ def predicted():
     # input_news = request.form["news"]
     input_news = request.json.get('news')
     output = load_model.predict([input_news])
-    return jsonify({'prediction': "The News is: {}".format(output) })
+    return jsonify({'prediction': "The News is:{} ".format(output) })
     
     # return render_template('index.html', prediction_text='The News is: {}'.format(output))
 
